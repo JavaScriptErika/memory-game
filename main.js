94cdbf2908371d1c166fc00c050dbeec93b8f6c4
JavaScriptErika <erikajritter@gmail.com>
2018-05-08 22:25:24 -0400

    refactor: remove unused vars in js

--- main.js
+++ main.js
@@ -2,27 +2,30 @@ const cardsArr = Array.from(document.querySelectorAll('.card'));
 
 let emojiArr = [];
 let clickCounter = 0;
-let indexEvenClick = 0;
 let indexOddClick = 0;
 
-let assignClicksToIndex = i => {
-  clickCounter % 2 === 0 ? (indexEvenClick = i) : (indexOddClick = i);
-};
+(() => {
+  cardsArr.forEach((card, i) => {
+    emojiArr.push(card.innerText);
 
-cardsArr.forEach((card, i) => {
-  emojiArr.push(card.innerText);
+    card.addEventListener('click', () => {
+      clickCounter++;
+      card.classList.add('flip-card');
 
-  card.addEventListener('click', () => {
-    clickCounter++;
-    card.classList.add('flip-card');
-
-    assignClicksToIndex(i);
-
-    if (emojiArr[`${indexEvenClick}`] === card.innerText && emojiArr[`${indexOddClick}`] !== card.innerText) {
-      setTimeout(() => {
-        card.classList.remove('flip-card');
-        cardsArr[`${indexOddClick}`].classList.remove('flip-card');
-      }, 800);
-    }
+      assignClicksToIndex(i);
+      ifCardsDontMatch(card);
+    });
   });
-});
+})();
+
+const assignClicksToIndex = i => (clickCounter % 2 === 0 ? null : (indexOddClick = i));
+
+const ifCardsDontMatch = card => {
+  console.log(indexOddClick);
+  if (emojiArr[`${indexOddClick}`] !== card.innerText) {
+    setTimeout(() => {
+      card.classList.remove('flip-card');
+      cardsArr[`${indexOddClick}`].classList.remove('flip-card');
+    }, 800);
+  }
+};
