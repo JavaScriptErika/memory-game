aff4dc16f63800cd94411bccc4428485f64f70be
JavaScriptErika <erikajritter@gmail.com>
2018-05-08 23:26:32 -0400

    refactor: add comments, rename function in js

--- main.js
+++ main.js
@@ -2,30 +2,41 @@ const cardsArr = Array.from(document.querySelectorAll('.card'));
 
 let emojiArr = [];
 let clickCounter = 0;
-let indexOddClick = 0;
+let indexFromClick = 0;
 
-(() => {
-  cardsArr.forEach((card, i) => {
-    emojiArr.push(card.innerText);
+/*
+IIFEE
+1) Loop through card array and add its emoji to a new array, emojiArr
 
-    card.addEventListener('click', () => {
-      clickCounter++;
-      card.classList.add('flip-card');
+Click Handler
+1) Add click handler to each of the card elements
+2) Keep count of times card is clicked,
+3) Add class to reveal back of card aka emoji
+*/
 
-      assignClicksToIndex(i);
-      ifCardsDontMatch(card);
-    });
+cardsArr.forEach((card, i) => {
+  emojiArr.push(card.innerText);
+
+  card.addEventListener('click', () => {
+    clickCounter++;
+    card.classList.add('flip-card');
+
+    trackClicksAssignIndex(i);
+    ifCardsDontMatch(card);
   });
-})();
+});
 
-const assignClicksToIndex = i => (clickCounter % 2 === 0 ? null : (indexOddClick = i));
+// First user click and every other user click will be assigned that corresponding index of card
+
+const trackClicksAssignIndex = i => (clickCounter % 2 === 0 ? null : (indexFromClick = i));
+
+// Compare the value of the emojiArr via index to card currently clicked from the event handler
 
 const ifCardsDontMatch = card => {
-  console.log(indexOddClick);
-  if (emojiArr[`${indexOddClick}`] !== card.innerText) {
+  if (emojiArr[`${indexFromClick}`] !== card.innerText) {
     setTimeout(() => {
       card.classList.remove('flip-card');
-      cardsArr[`${indexOddClick}`].classList.remove('flip-card');
+      cardsArr[`${indexFromClick}`].classList.remove('flip-card');
     }, 800);
   }
 };
