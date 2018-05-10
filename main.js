6bb8d4843ce6a285cb53c88b832ade8d63f0372e
JavaScriptErika <erikajritter@gmail.com>
2018-05-10 17:06:48 -0400

    refactor: rearrange functions, improve moves counter

--- main.js
+++ main.js
@@ -9,6 +9,42 @@ const playerTimer = document.getElementById("playerTimer")
 let tempOpenCards = [];
 
 /*
+Load Handler
+1) Call function to randomly shuffle the indexes of the emoji array
+2) Display JS-created cards with randomly shuffled emoji
+3) Grab all cards from the DOM, turn Nodelist into array
+4) Call function add click handler to cards in DOM
+*/
+window.addEventListener("load", () => {
+  shuffle(emojiArr);
+  displayCards();
+  cardsArr = Array.from(document.querySelectorAll(".card"));
+  addClickListenerToCards(cardsArr);
+  displayPlayerMoves(true)
+})
+
+
+/*
+Shuffle
+1) Randomly shuffle the indexes of emojiArr
+2) Credit http://stackoverflow.com/a/2450976
+*/
+const shuffle = (array) => {
+
+  let currentIndex = array.length, temporaryValue, randomIndex;
+
+  while (currentIndex !== 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+  return array;
+}
+
+
+/*
 Display JS-created cards with randomly shuffled emojiArr
 Invoked by load handler
 */
@@ -30,30 +66,13 @@ const displayCards = () => {
   }
 }
 
-/*
-Shuffle
-1) Randomly shuffle the indexes of emojiArr
-2) Credit http://stackoverflow.com/a/2450976
-*/
-const shuffle = (array) => {
-
-  let currentIndex = array.length, temporaryValue, randomIndex;
-
-  while (currentIndex !== 0) {
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex -= 1;
-    temporaryValue = array[currentIndex];
-    array[currentIndex] = array[randomIndex];
-    array[randomIndex] = temporaryValue;
-  }
-  return array;
-}
 
 /*
 Click Handler
 1) Add click handler to each of the card elements
 2) Keep count of games moves
-3) Keep count of card clicks so that users can't exceed
+3) Flip and reset cards back if a user clicks multiple times on 1 card
+4) Keep count of card clicks so that users can't exceed
 more than 2 card flips until the cardClicksCounter is reset 
 when cards do or don't match
 */
@@ -61,18 +80,24 @@ when cards do or don't match
 const addClickListenerToCards = (cardsArr) => {
   cardsArr.forEach((card, i) => {
     card.addEventListener("click", () => {
-      displayPlayerMoves();
+
       cardClicksCounter++
 
-      cardClicksCounter <= 2 ? revealCardBack(card) : null;
+      if (card.classList.contains('flip-card')) {
+        cardsDontMatch()
+      } else {
+        cardClicksCounter <= 2 ? revealCardBack(card) : null;
+      }
+
     }, false);
   });
 }
 
-const displayPlayerMoves = () => {
-  // gameMovesCounter++;
-  playerMoves.textContent = `${gameMovesCounter++}`
+const displayPlayerMoves = (keepCounting) => {
+  keepCounting ? playerMoves.textContent = `${gameMovesCounter++}` : null;
+
 }
+
 
 /*
 1) Add a transition class to reveal emoji
@@ -91,21 +116,16 @@ const revealCardBack = (card) => {
 2) Make sure two cards have been clicked and compare if they match
 */
 const addOpenCardstoTempArr = (card) => {
+  displayPlayerMoves(true);
   tempOpenCards.push(card.innerText)
   tempOpenCards.length === 2 ? doCardsMatch(card) : null;
-}
-
-// Resets cardClicksCounter after 1s
-const resetCardClicks = () => {
-  setTimeout(() => {
-    cardClicksCounter = 0;
-  }, 1000)
 }
 
 /*
 1) Compare previous card clicked [0] and current card clicked [1]
 */
 const doCardsMatch = (card) => {
+
   tempOpenCards[0] === tempOpenCards[1] ? cardsDoMatch() : cardsDontMatch(card)
 }
 
@@ -121,10 +141,16 @@ we do not keep adding them to the matchingCards array.
 */
 const cardsDoMatch = () => {
   matchingCards.includes(tempOpenCards[1]) ? null : matchingCards.push(tempOpenCards[0], tempOpenCards[1]);
-  console.log(matchingCards)
-  resetCardClicks();
 
+  resetCardClicks();
   clearTempArr()
+}
+
+// Resets cardClicksCounter after 1s
+const resetCardClicks = () => {
+  setTimeout(() => {
+    cardClicksCounter = 0;
+  }, 1000)
 }
 
 /*
@@ -145,6 +171,7 @@ in the matchingCards array
 */
 const cardsDontMatch = (card) => {
   clearTempArr()
+  displayPlayerMoves(false);
   setTimeout(() => {
     cardsArr.forEach((card) => {
       if (!matchingCards.includes(card.innerText)) {
@@ -155,17 +182,8 @@ const cardsDontMatch = (card) => {
   resetCardClicks();
 }
 
-/*
-Load Handler
-1) Call function to randomly shuffle the indexes of the emoji array
-2) Display JS-created cards with randomly shuffled emoji
-3) Grab all cards from the DOM, turn Nodelist into array
-4) Call function add click handler to cards in DOM
-*/
-window.addEventListener("load", () => {
-  shuffle(emojiArr);
-  displayCards();
-  cardsArr = Array.from(document.querySelectorAll(".card"));
-  addClickListenerToCards(cardsArr);
-  displayPlayerMoves()
-})
+
+
+const gameTimer = () => {
+
+}
